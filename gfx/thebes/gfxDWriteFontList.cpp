8fa07a521e9755d8d0f75abf2537f7bdd6982f7a
Lando <lando@lando.moz.tools>
2026-03-25 04:48:56 +0000

    Bug 2015952: apply code formatting via Lando
    # ignore-this-changeset

--- gfx/thebes/gfxDWriteFontList.cpp
+++ gfx/thebes/gfxDWriteFontList.cpp
@@ -512,7 +512,8 @@ hb_blob_t* gfxDWriteFontEntry::GetFontTable(uint32_t aTag) {
   return nullptr;
 }
 
-gfxFontEntry::FontTableCache* gfxDWriteFontEntry::GetFontTableCache(bool aCreate) {
+gfxFontEntry::FontTableCache* gfxDWriteFontEntry::GetFontTableCache(
+    bool aCreate) {
   // Create the cache if it does not yet exist.
   if (!mFontTableCache && aCreate) {
     auto* cache = new FontTableCache();
