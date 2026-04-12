e40fbe050fcd01dc5f99a5c77b6d5387bcd38a12
rperta <rperta@mozilla.com>
2026-04-12 22:29:01 +0300

    Revert "Bug 1998985 - Convert XUL in SearchModeSwitcher to HTML. r=fluent-reviewers,desktop-theme-reviewers,tabbrowser-reviewers,urlbar-reviewers,tgiles,bolsson,dao" for causing assertion failures at BindingUtils.cpp
    This reverts commit d472cdc25d3fbd844d80b4e9ce139daad49814f2.

--- browser/base/content/browser-toolbarKeyNav.js
+++ browser/base/content/browser-toolbarKeyNav.js
@@ -37,9 +37,7 @@ ToolbarKeyboardNavigator = {
       return false;
     }
     return (
-      aElem.tagName == "toolbarbutton" ||
-      aElem.tagName == "html:moz-button" ||
-      aElem.getAttribute("role") == "button"
+      aElem.tagName == "toolbarbutton" || aElem.getAttribute("role") == "button"
     );
   },
 
@@ -387,12 +385,6 @@ ToolbarKeyboardNavigator = {
 
     if (focus.getAttribute("type") == "menu") {
       focus.open = true;
-      return;
-    }
-
-    // moz-button uses an html button internally
-    // which already receives a click event on enter.
-    if (focus.localName == "moz-button") {
       return;
     }
 
